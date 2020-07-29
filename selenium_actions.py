dd276508ef631f5e67e7814a033ebe94d7268fb8
qamay204 <moisieievtest@gmail.com>
2020-07-29 15:58:42 +0300

    hw advaced add

--- selenium_actions.py
+++ selenium_actions.py
@@ -15,13 +15,73 @@ Check next test cases:
 6. Find worker in search field and edit it
 7. Validate data in worker row after creating worker
 8. Check search by all column values
-
 https://demoqa.com/webtables
 """
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 import unittest
 import time
+
+
+def wait_until_element_is_visible(driver, selector, seconds=20):
+    WebDriverWait(driver, seconds).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, selector))
+    )
+
+
+def check_selected_option(select_tag):
+    select = Select(select_tag)
+    selected_option = select.first_selected_option
+    return selected_option.text
+
+
+def add_worker(driver, w_id=0):
+    driver.find_element_by_id('addNewRecordButton').click()
+    wait_until_element_is_visible(driver, '[class=modal-content]', 5)
+    driver.find_element_by_id('firstName').send_keys('Andrii' + str(w_id))
+    driver.find_element_by_id('lastName').send_keys('Moisieiev' + str(w_id))
+    driver.find_element_by_id('userEmail').send_keys('moisieiev@gmail.com')
+    driver.find_element_by_id('age').send_keys('11')
+    driver.find_element_by_id('salary').send_keys('1111')
+    driver.find_element_by_id('department').send_keys('QA')
+    driver.find_element_by_id('submit').click()
+
+
+def check_added_worker(driver, xpath_str):
+    elem_for_check = driver.find_element_by_xpath(xpath_str)
+    if not elem_for_check.find_elements_by_css_selector('.rt-td')[0].text == "Andrii0":
+        return False
+    elif not elem_for_check.find_elements_by_css_selector('.rt-td')[0].text == "Andrii0":
+        return False
+    elif not elem_for_check.find_elements_by_css_selector('.rt-td')[1].text == "Moisieiev0":
+        return False
+    elif not elem_for_check.find_elements_by_css_selector('.rt-td')[2].text == "11":
+        return False
+    elif not elem_for_check.find_elements_by_css_selector('.rt-td')[3].text == "moisieiev@gmail.com":
+        return False
+    elif not elem_for_check.find_elements_by_css_selector('.rt-td')[4].text == "1111":
+        return False
+    elif not elem_for_check.find_elements_by_css_selector('.rt-td')[5].text == "QA":
+        return False
+    return True
+
+
+def del_record(driver, css_selector_str):
+    elements = driver.find_elements_by_css_selector(css_selector_str)
+    print("Elements:  ", elements)
+    print("Elements len:  ", len(elements))
+    if not elements:
+        return 'Can\'t find elements'
+
+    for i in range(len(elements)):
+        elem = driver.find_element_by_css_selector(css_selector_str)
+        elem.click()
+        # time.sleep(2)
+    del_record(driver, css_selector_str)
 
 
 class TestBasicLevel(unittest.TestCase):
@@ -30,7 +90,7 @@ class TestBasicLevel(unittest.TestCase):
         self.chrome_options.add_argument('headless')
         # self.driver = webdriver.Chrome(options=self.chrome_options)
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(10000)
+        self.driver.implicitly_wait(1)
         self.driver.set_window_size(1920, 1080)
 
     def tearDown(self) -> None:
@@ -59,7 +119,21 @@ class TestBasicLevel(unittest.TestCase):
         driver.find_element_by_id('submit').click()
         self.assertTrue(driver.find_element_by_css_selector('.modal-open'))
 
-
+    def test_advanced_form(self):
+        driver = self.driver
+        driver.get('https://demoqa.com/webtables')
+        driver.find_element_by_xpath('//option[@value=5]').click()
+        check_selected_option(driver.find_element_by_xpath('//select'))
+        self.assertEqual('5 rows', check_selected_option(driver.find_element_by_xpath('//select')))
+        for i in range(3):
+            add_worker(driver, i)
+        driver.find_element_by_xpath('//button[contains(., "Next")]').click()
+        self.assertEqual(driver.find_element_by_css_selector('[aria-label="jump to page"]').get_attribute('value'), '2')
+        driver.find_element_by_xpath('//button[contains(., "Previous")]').click()
+        del_record(driver, "[id*='delete-record']")
+        add_worker(driver)
+        self.assertTrue(check_added_worker(driver, '//div[@class="rt-tr-group"][1]'))
+        # driver.find_element_by_id('dsdsdsd')
 
 
 if __name__ == '__main__':
