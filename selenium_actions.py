63921ab387440750107c05b75a3ad19700a436a9
brainwashers <oliinyk.ihor@gmail.com>
2020-07-30 13:20:50 +0300

    Selenium Act

--- selenium_actions.py
+++ selenium_actions.py
@@ -18,3 +18,35 @@ Check next test cases:
 
 https://demoqa.com/webtables
 """
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+TEST_DATA = {
+            'First Name': 'Jorge',
+            'Last Name': 'Lopes',
+            'Email': 'jorgeL@gmail.com',
+            'Phone': '1234567890',
+            'HB': '07 Jul 1985',
+            'Address': 'ARAMARK Ltd. 30 Commercial Road Fratton PORTSMOUTH Hampshire PO1 1AA UNITED KINGDOM'
+}
+
+driver = webdriver.Chrome()
+driver.get("https://demoqa.com/automation-practice-form")
+driver.find_element_by_css_selector("#firstName").send_keys(TEST_DATA['First Name'])
+driver.find_element(By.CSS_SELECTOR, "#lastName").send_keys(TEST_DATA['Last Name'])
+driver.find_element(By.CSS_SELECTOR, "#userEmail").send_keys(TEST_DATA['Email'])
+driver.find_element(By.CSS_SELECTOR, "#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label").click()
+driver.find_element(By.CSS_SELECTOR, "#userNumber").send_keys(TEST_DATA['Phone'])
+calendar_input = driver.find_element(By.CSS_SELECTOR, "#dateOfBirthInput")
+calendar_input.send_keys(Keys.LEFT_CONTROL, 'a')
+calendar_input.send_keys('07 Jul 1985', Keys.ENTER)
+driver.find_element(By.CSS_SELECTOR, "#subjectsInput").send_keys('Comp')
+driver.find_element(By.CSS_SELECTOR, "#subjectsInput").send_keys(Keys.ENTER)
+driver.find_element_by_id("hobbies-checkbox-3").send_keys(Keys.SPACE)
+driver.find_element(By.CSS_SELECTOR, "#currentAddress").send_keys(TEST_DATA['Address'])
+driver.find_element_by_css_selector("#react-select-3-input").send_keys('utt', Keys.ENTER)
+driver.find_element_by_css_selector("#react-select-4-input").send_keys('mer', Keys.ENTER)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+driver.find_element_by_css_selector("#submit").click()
+assert "Thanks for submitting the form" == driver.find_element_by_css_selector("#example-modal-sizes-title-lg").text
+driver.quit()
