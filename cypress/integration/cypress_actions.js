1707357b3938daa3c0c0a182f2ff3ef09d9fa65a
OlenaKamenetska <olena.kamen@gmail.com>
2020-10-28 16:30:48 +0200

    Home_work_36

--- cypress/integration/cypress_actions.js
+++ cypress/integration/cypress_actions.js
@@ -4,15 +4,20 @@
 // 3. Validate inputed data in modal window
 // Site: https://demoqa.com/automation-practice-form
 
-// Advanced level:
-// Check next test cases:
-// 1. Pagination 
-// 2. Rows count selection
-// 3. Add new worker
-// 4. Delete worker
-// 5. Delete all worker
-// 6. Find worker in search field and edit it
-// 7. Validate data in worker row after creating worker
-// 8. Check search by all column values
+it('Student_Registration_Form', () => {
+    cy.visit('https://demoqa.com/automation-practice-form');
 
-// https://demoqa.com/webtables
+    cy.get('#firstName').type('Olena');
+    cy.get('#lastName').type('Kamen');  
+    cy.get('#userEmail').type('ok@gmail.com');   
+    cy.get('[class="col-md-9 col-sm-12"]').contains('Female').click();
+    cy.get('#userNumber').type('380952252255');
+    cy.get('#dateOfBirthInput').type('{selectall}28 sept 1950{enter}');
+    cy.get('#subjectsContainer').type('Math{enter}');   
+    cy.get('[class="col-md-9 col-sm-12"]').contains('Sports').click();
+    cy.get('#currentAddress').type('Kharkiv, Ukraine, Pushkinska str');   
+    cy.get('#state').contains('Select State').type('NCR{enter}');
+    cy.get('#city').contains('Select City').type('Delhi{enter}');
+    cy.get('[class="btn btn-primary"]').click();
+});
+    
