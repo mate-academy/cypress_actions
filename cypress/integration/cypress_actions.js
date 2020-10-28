3df8509471869690c002f91261c58c22142a225c
AlyonaKobzeva <alena.kobzeva11@gmail.com>
2020-10-28 16:52:43 +0200

    AlyonaKobzieva

--- cypress/integration/cypress_actions.js
+++ cypress/integration/cypress_actions.js
@@ -4,15 +4,31 @@
 // 3. Validate inputed data in modal window
 // Site: https://demoqa.com/automation-practice-form
 
-// Advanced level:
-// Check next test cases:
-// 1. Pagination 
-// 2. Rows count selection
-// 3. Add new worker
-// 4. Delete worker
-// 5. Delete all worker
-// 6. Find worker in search field and edit it
-// 7. Validate data in worker row after creating worker
-// 8. Check search by all column values
+it('Registration form', () => {
+  cy.visit('https://demoqa.com/automation-practice-form');
+  cy.get('[placeholder="First Name"]').type('Alyona');
+  cy.get('[placeholder="Last Name"]').type('Kobzieva');
+  cy.get('[placeholder="name@example.com"]').type('alena123@mail.ua');
+  cy.get('[class="custom-control custom-radio custom-control-inline"]').contains('Female').click();
+  cy.get('[placeholder="Mobile Number"]').type('0681234567');
+  cy.get('#dateOfBirthInput').type('{selectall}').type('11 Jan 2000').type('{enter}');;
+  cy.get('#subjectsInput').type('Mat{enter}');
+  cy.get('[class="custom-control custom-checkbox custom-control-inline"]').contains('Sports').click();
+  cy.get('[placeholder="Current Address"]').type('Kyiv');
+  cy.get('#react-select-3-input').type('U{enter}', {force: true});
+  cy.get('#react-select-4-input').type('M{enter}', {force: true});
+  cy.get('[type="submit"]').click();
+});
 
-// https://demoqa.com/webtables
+it('Validate', () => {
+  cy.get('[class="table-responsive"]').type('Alyona').should('exist');
+  cy.get('[class="table-responsive"]').type('Kobzieva').should('exist');
+  cy.get('[class="table-responsive"]').type('Kobzieva').should('exist');
+  cy.get('[class="table-responsive"]').type('alena123@mail.ua').should('exist');
+  cy.get('[class="table-responsive"]').type('Female').should('exist');
+  cy.get('[class="table-responsive"]').type('11 Jan 2000').should('exist');
+  cy.get('[class="table-responsive"]').type('Maths').should('exist');
+  cy.get('[class="table-responsive"]').type('Sports').should('exist');
+  cy.get('[class="table-responsive"]').type('Kyiv').should('exist');
+  cy.get('[class="table-responsive"]').type('Uttar Predesh Merrut').should('exist');
+});
